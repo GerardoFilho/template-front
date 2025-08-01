ef55929dee293d6af5e985ee3e92bc1469a68b52
Edson Cândido <edson.alves.millenium@caixaconsorcio.com.br>
2025-08-01 16:10:13 -0300

    feat: config query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import AppRoutes from "./routes";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './services/queryClient';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  )
 }
 
 export default App;
